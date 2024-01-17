2a4f63302886a8d99126b0aa74bf1ce712c03fa9
BNEEE <bneeeee0@gmail.com>
2024-01-17 06:41:38 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter ,Routes , Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+
+
+const App = () => {
+  return (
+
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="Contact" element ={<Contact/>} />
+        <Route path="Portfolio" element ={<Portfolio/>} />
+        <Route path="About" element ={<About/>} />
+
+      </Routes>
+      
+      
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Home.css'
+import profile from "../../assets/BNE.jpeg"
+import { Link } from 'react-router-dom';
+import { FaArrowCircleRight } from "react-icons/fa";
+
+const Home = () => {
+  return (
+   <section className="home section grid">
+    <img src={profile} alt="" className="home_img" />
+    <div className="home__content">
+     <div className="home__data">
+      <h1 className="home__title">
+        <span>I am Beshoy Nasry </span> Full stack developer   <span>  MERN </span>
+      </h1>
+      <p className="home__description">
+      MERN stack developer specializes in full-stack web development, leveraging MongoDB for data storage, Express.js for server-side application building, React for dynamic front-end interfaces, and Node.js for server-side JavaScript execution. Proficient in end-to-end development, they create robust, scalable web applications by seamlessly integrating these technologies throughout the development lifecycle.
+      </p>
+      <Link to ='/About' className="button">
+          More About Me <span className="button__icon">
+            <FaArrowCircleRight/>
+          </span>
+      </Link>
+     </div>
+
+    </div>
+   </section>
+  );
+};
+
+export default Home;
